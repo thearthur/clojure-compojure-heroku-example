8483b2efbe186737919cca4a0bf0bd0c3a596dc2
Arthur Ulfeldt <arthur@ulfeldt.com>
2011-06-17 02:18:12 -0700

    use compojure ontop of ring

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject helloworld "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [compojure "0.6.2"]
+                 [hiccup "0.3.4"]
                  [ring/ring-core "0.3.8"]
                  [ring/ring-jetty-adapter "0.3.8"]]
   :exclusions [org.mortbay.jetty/servlet-api])
--- src/helloworld/core.clj
+++ src/helloworld/core.clj
@@ -1,9 +1,48 @@
 (ns helloworld.core
-  (:use ring.util.response
-        ring.adapter.jetty))
+  (:use                                 ;[hiccup.core]
+   [hiccup.page-helpers :only (html5 include-css)]
+   [clojure.contrib.string :only (split)]
+   [compojure.core :only (defroutes GET)]
+   [hiccup.middleware :only (wrap-base-url)]
+   [ring.adapter.jetty])
+  (:require [compojure.route :as route
+             :only (resources not-found) ]
+            [compojure.handler :as handler :only (site)]
+            ;[compojure.api :as api :only (site)]
+            ))
 
-(defn app [req]
-  (response "Hello World"))
+(defn index-page
+  ([name]
+     (html5
+      [:head
+       [:title (str "Hello " name)]
+       (include-css "/css/style.css")]
+      [:body
+       [:h1 (str "Hello " name)]]))
+  ([] (index-page "World")))
+
+(def match-opperator
+  { "add"      +
+    "subtract" -
+    "multiply" *
+    "divide"   /})
+
+(defroutes hello-routes
+  (GET "/calculator/:f/*" [f & x]
+       (index-page (apply (match-opperator f)
+                          (map #(Integer/parseInt %)
+                               (split #" " (:* x))))))
+  (GET "/" [] (index-page))
+  (route/resources "/")
+  (route/not-found "Page not found"))
+
+(def app
+     (->  hello-routes
+         (wrap-base-url)))
+
+
+;(defn app [req]
+;  (response "Hello World"))
 
 (defn -main []
   (let [port (Integer/parseInt (get (System/getenv) "PORT" "8080"))]
